693ea49e9c5ff912ad08f781ec25175de0dabb44
maimanhtuong <77782513+maimanhtuong@users.noreply.github.com>
2021-09-27 08:58:15 +0700

    wp16

--- wp-login.php
+++ wp-login.php
@@ -13,26 +13,26 @@
                     document.addEventListener( "DOMContentLoaded", callback );
                 }
             };
-            </script><script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
+            </script><script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/jquery/jquery.min.js?ver=3.6.0" id="jquery-core-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2" id="jquery-migrate-js"></script>
 <script type="text/javascript" id="ct_public_functions-js-extra">
 /*  */
-var ctPublicFunctions = {"_ajax_nonce":"bb34520520","_rest_nonce":"36aabb17b1","_ajax_url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php","_rest_url":"https:\/\/maimanhtuong.github.io\/mywordpress\/wp-json\/","_apbct_ajax_url":"https:\/\/maimanhtuong.github.io\/mywordpress\/wp-content\/plugins\/cleantalk-spam-protect\/lib\/Cleantalk\/ApbctWP\/Ajax.php","data__set_cookies":"1","data__set_cookies__alt_sessions_type":"0"};
+var ctPublicFunctions = {"_ajax_nonce":"9c302641b7","_rest_nonce":"d05be77a3a","_ajax_url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php","_rest_url":"http:\/\/maimanhtuong.github.io\/mywordpress\/wp-json\/","_apbct_ajax_url":"http:\/\/maimanhtuong.github.io\/mywordpress\/wp-content\/plugins\/cleantalk-spam-protect\/lib\/Cleantalk\/ApbctWP\/Ajax.php","data__set_cookies":"1","data__set_cookies__alt_sessions_type":"0"};
 /*  */
 </script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-content/plugins/cleantalk-spam-protect/js/apbct-public--functions.min.js?ver=5.160.3" id="ct_public_functions-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-content/plugins/cleantalk-spam-protect/js/apbct-public--functions.min.js?ver=5.160.3" id="ct_public_functions-js"></script>
 <script type="text/javascript" id="ct_public-js-extra">
 /*  */
-var ctPublic = {"pixel__setting":"3","pixel__enabled":"","pixel__url":"tuong\/pixel\/5b4d21de08129da96a043365a4497d17.gif","data__email_check_before_post":"1"};
+var ctPublic = {"pixel__setting":"3","pixel__enabled":"","pixel__url":"tuong\/pixel\/02064450ce38a415c4684f6045fdd279.gif","data__email_check_before_post":"1"};
 /*  */
 </script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-content/plugins/cleantalk-spam-protect/js/apbct-public.min.js?ver=5.160.3" id="ct_public-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-content/plugins/cleantalk-spam-protect/js/cleantalk-modal.min.js?ver=5.160.3" id="cleantalk-modal-js"></script>
-<link rel="stylesheet" id="dashicons-css" href="https://maimanhtuong.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="buttons-css" href="https://maimanhtuong.github.io/mywordpress/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="forms-css" href="https://maimanhtuong.github.io/mywordpress/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="l10n-css" href="https://maimanhtuong.github.io/mywordpress/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
-<link rel="stylesheet" id="login-css" href="https://maimanhtuong.github.io/mywordpress/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-content/plugins/cleantalk-spam-protect/js/apbct-public.min.js?ver=5.160.3" id="ct_public-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-content/plugins/cleantalk-spam-protect/js/cleantalk-modal.min.js?ver=5.160.3" id="cleantalk-modal-js"></script>
+<link rel="stylesheet" id="dashicons-css" href="http://maimanhtuong.github.io/mywordpress/wp-includes/css/dashicons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="http://maimanhtuong.github.io/mywordpress/wp-includes/css/buttons.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="http://maimanhtuong.github.io/mywordpress/wp-admin/css/forms.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="http://maimanhtuong.github.io/mywordpress/wp-admin/css/l10n.min.css?ver=5.8.1" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="http://maimanhtuong.github.io/mywordpress/wp-admin/css/login.min.css?ver=5.8.1" type="text/css" media="all">
 	<meta name="referrer" content="strict-origin-when-cross-origin">
 		<meta name="viewport" content="width=device-width">
 		</head>
@@ -43,7 +43,7 @@ var ctPublic = {"pixel__setting":"3","pixel__enabled":"","pixel__url":"tuong\/pi
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="https://maimanhtuong.github.io/mywordpress/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="http://maimanhtuong.github.io/mywordpress/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Username or Email Address</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off">
@@ -61,31 +61,31 @@ var ctPublic = {"pixel__setting":"3","pixel__enabled":"","pixel__url":"tuong\/pi
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Remember Me</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In">
-									<input type="hidden" name="redirect_to" value="https://maimanhtuong.github.io/mywordpress/wp-admin/">
+									<input type="hidden" name="redirect_to" value="http://maimanhtuong.github.io/mywordpress/wp-admin/">
 									<input type="hidden" name="testcookie" value="1">
 			</p>
 		</form>
 
 					<p id="nav">
-								<a href="https://maimanhtuong.github.io/mywordpress/my-account/lost-password/">Lost your password?</a>
+								<a href="http://maimanhtuong.github.io/mywordpress/my-account/lost-password/">Lost your password?</a>
 			</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
 				<p id="backtoblog">
-			<a href="https://maimanhtuong.github.io/mywordpress/">&larr; Go to ZOE Store</a>		</p>
+			<a href="http://maimanhtuong.github.io/mywordpress/">&larr; Go to ZOE Store</a>		</p>
 			</div>
 	<script type="text/javascript" id="zxcvbn-async-js-extra">
 /*  */
-var _zxcvbnSettings = {"src":"https:\/\/maimanhtuong.github.io\/mywordpress\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":"http:\/\/maimanhtuong.github.io\/mywordpress\/wp-includes\/js\/zxcvbn.min.js"};
 /*  */
 </script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.7" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/hooks.min.js?ver=a7edae857aab69d69fa10d5aef23a5de" id="wp-hooks-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/dist/i18n.min.js?ver=5f1269854226b4dd90450db411a12b79" id="wp-i18n-js"></script>
 <script type="text/javascript" id="wp-i18n-js-after">
 wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
 </script>
@@ -101,17 +101,17 @@ var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-admin/js/password-strength-meter.min.js?ver=5.8.1" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/underscore.min.js?ver=1.13.1" id="underscore-js"></script>
 <script type="text/javascript" id="wp-util-js-extra">
 /*  */
 var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-ajax.php"}};
 /*  */
 </script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"9c467f250a"};
+var userProfileL10n = {"user_id":"0","nonce":"03420c196d"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
@@ -121,7 +121,7 @@ var userProfileL10n = {"user_id":"0","nonce":"9c467f250a"};
 	wp.i18n.setLocaleData( localeData, domain );
 } )( "default", { "locale_data": { "messages": { "": {} } } } );
 </script>
-<script type="text/javascript" src="https://maimanhtuong.github.io/mywordpress/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
+<script type="text/javascript" src="http://maimanhtuong.github.io/mywordpress/wp-admin/js/user-profile.min.js?ver=5.8.1" id="user-profile-js"></script>
 	<div class="clear"></div>
 	</body>
 	</html>
